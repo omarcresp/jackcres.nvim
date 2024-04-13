17545d982d8f767639207751b40df639261ce66f
Omar Crespo <crespomerchano@gmail.com>
2024-04-13 16:37:15 -0500

    update git motions

--- lua/jackcres/plugins/fugitive.lua
+++ lua/jackcres/plugins/fugitive.lua
@@ -6,6 +6,7 @@ return {
         set("n", "<leader>gs", vim.cmd.Git, { desc = "[G]it [S]tatus" })
         set("n", "<leader>gB", "<cmd>G blame<CR>", { desc = "[G]it [B]lame file" })
         set("n", "<leader>gl", "<cmd>G log<CR>", { desc = "[G]it [L]og" })
+        -- TODO: create alternatives of git push that allows remote set branch
         set("n", "<leader>gP", "<cmd>G push<CR>", { desc = "[G]it [P]ush" })
         set("n", "<leader>gp", "<cmd>G pull<CR>", { desc = "[G]it [P]ull" })
 
