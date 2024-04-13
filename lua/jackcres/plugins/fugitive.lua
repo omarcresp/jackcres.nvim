65dd84e7d9d42b9d92e45fd07ec662e65a1af16b
Omar Crespo <crespomerchano@gmail.com>
2024-04-12 21:36:27 -0500

    add more git bindings

--- lua/jackcres/plugins/fugitive.lua
+++ lua/jackcres/plugins/fugitive.lua
@@ -1,7 +1,13 @@
 return {
-  'tpope/vim-fugitive',
-  config = function()
-    vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = '[G]it [S]tatus' })
-    vim.keymap.set("n", "<leader>gb", "<cmd>G branch<CR>", { desc = '[G]it [B]ranch' })
-  end,
+    "tpope/vim-fugitive",
+    config = function()
+        local set = vim.keymap.set
+
+        set("n", "<leader>gs", vim.cmd.Git, { desc = "[G]it [S]tatus" })
+        set("n", "<leader>gb", "<cmd>G branch<CR>", { desc = "[G]it [B]ranch" })
+        set("n", "<leader>gd", "<cmd>G diff<CR>", { desc = "[G]it [D]iff" })
+        set("n", "<leader>gl", "<cmd>G log<CR>", { desc = "[G]it [L]og" })
+        set("n", "<leader>gP", "<cmd>G push<CR>", { desc = "[G]it [P]ush" })
+        set("n", "<leader>gp", "<cmd>G pull<CR>", { desc = "[G]it [P]ull" })
+    end,
 }
