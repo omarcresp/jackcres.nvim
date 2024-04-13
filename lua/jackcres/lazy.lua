3fc549aece9c735d998ca0bf92b75a8d2c3ed206
Omar Crespo <crespomerchano@gmail.com>
2024-04-12 23:10:38 -0500

    update lazy vim install part

--- lua/jackcres/lazy.lua
+++ lua/jackcres/lazy.lua
@@ -1,12 +1,15 @@
 -- [[ Install `lazy.nvim` plugin manager ]]
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
-if not vim.loop.fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    vim.fn.system({ "git", "clone", "--depth=1 --filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
-
----@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 -- install lazy plugins binding
